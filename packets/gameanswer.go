c61aec98021c30f54951e9e3c7db5b71383f7e1d
Captain ALM <captainalm@captainalm.com>
2024-01-03 15:53:51 +0000

    Add a question number to the game answer payload.

--- packets/gameanswer.go
+++ packets/gameanswer.go
@@ -8,12 +8,13 @@ import (
 // GameAnswer Sent from app server to web client
 const GameAnswer = "agame"
 
-func NewGameAnswer(index uint32, key *rsa.PrivateKey) (*packet.Packet, error) {
-	return packet.New(GameAnswer, &GameAnswerPayload{index}, key)
+func NewGameAnswer(index uint32, questionNumber uint32, key *rsa.PrivateKey) (*packet.Packet, error) {
+	return packet.New(GameAnswer, &GameAnswerPayload{questionNumber, index}, key)
 }
 
 type GameAnswerPayload struct {
-	Index uint32 `json:"x"`
+	QuestionNumber uint32 `yaml:"q"`
+	Index          uint32 `json:"x"`
 }
 
 //The GameAnswerPayload is also in use by GameCommit
--- packets/gamecommit.go
+++ packets/gamecommit.go
@@ -8,8 +8,8 @@ import (
 // GameCommit Sent from web client to app server
 const GameCommit = "cgame"
 
-func NewGameCommit(index uint32, key *rsa.PrivateKey) (*packet.Packet, error) {
-	return packet.New(GameCommit, &GameAnswerPayload{index}, key)
+func NewGameCommit(index uint32, questionNumber uint32, key *rsa.PrivateKey) (*packet.Packet, error) {
+	return packet.New(GameCommit, &GameAnswerPayload{questionNumber, index}, key)
 }
 
 //This packet uses the GameAnswerPayload
