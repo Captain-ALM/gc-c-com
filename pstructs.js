8d01c77976722087807972cc578798bf42859f85
Captain ALM <captainalm@captainalm.com>
2024-01-10 16:26:29 +0000

    Fix new packet.

--- pstructs.js
+++ pstructs.js
@@ -252,7 +252,7 @@ function NewPacket(command) {
 	if (cAttrb.length > 0) {
 		pkToRet.payload = {TYPE: "payload"};
 		for (let i = 0; i < cAttrb.length; ++i) {
-			pkToRet.payload[cAttrb[1]] = undefined;
+			pkToRet.payload[cAttrb[i][1]] = undefined;
 		}
 	}
 	return pkToRet;
